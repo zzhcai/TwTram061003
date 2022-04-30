8e580d87d71f38a354996e61cccea6835362b879
zzhcai <87593086+zzhcai@users.noreply.github.com>
2022-04-30 19:26:18 +1000

    Debug instance creation

--- ansible/run-mrc.sh
+++ ansible/run-mrc.sh
@@ -5,4 +5,4 @@
 # Bingzhe Jin (1080774), Hongwei Chen (891632), Tian Hui (1054513),
 # Zhen Cai (1049487), Ziqi Zhang (1241157)
 
-. ./openrc.sh; ansible-playbook openstack.yaml
+. ./openrc.sh; ansible-playbook --ask-become-pass openstack.yaml
