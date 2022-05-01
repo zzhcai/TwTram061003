9dc320c380b43030ec0be21c0636409ecc489bec
hhh476-tian <58553923+hhh476-tian@users.noreply.github.com>
2022-05-01 21:50:03 +1000

    Add couchDB docker compose
    Co-Authored-By: Zhen Cai <87593086+zzhcai@users.noreply.github.com>

--- ansible/run-mrc.sh
+++ ansible/run-mrc.sh
@@ -5,4 +5,4 @@
 # Bingzhe Jin (1080774), Hongwei Chen (891632), Tian Hui (1054513),
 # Zhen Cai (1049487), Ziqi Zhang (1241157)
 
-. ./openrc.sh; ansible-playbook -u ubuntu --key-file=cloud.key --ask-become-pass openstack.yaml
+. ./openrc.sh; ansible-playbook -u ubuntu --key-file=cloud.key --ask-become-pass main.yaml
