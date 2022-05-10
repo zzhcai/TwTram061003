e47f6d008abc9990fbbe7e0aaa85e7509afa0177
Charles <HongweiChen38@gmail.com>
2022-05-11 00:15:08 +1000

    Google Maps API integration

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Assignment', key: "AIzaSyDqODW7h13yD9PzJ4wv2y8EJ_pdhy71vp0" });
 });
 
 module.exports = router;
