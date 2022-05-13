d1ddf9e3e5c8e8119b24d803b9e11d888c6bb205
hhh476-tian <58553923+hhh476-tian@users.noreply.github.com>
2022-05-13 21:51:21 +1000

    Add query in query search

--- TwitterHarvester/harvester2_search.sh
+++ TwitterHarvester/harvester2_search.sh
@@ -2,4 +2,4 @@ export query="("
 query+="$(python3 get_query_keywords.py -f "landmarks.txt")"
 query+=") (myki OR tram OR trams) lang:en -is:retweet"
 echo $query
-python3 search.py -q "(melbourne OR ) (myki OR tram OR trams) lang:en -is:retweet" -d melb_db -u user_db
+python3 search.py -q $query -d melb_db -u user_db
