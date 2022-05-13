b5c14d7f107aafed9f305ad300d00d28e317278b
zzhcai <87593086+zzhcai@users.noreply.github.com>
2022-05-13 20:15:42 +1000

    Create max/min polarity views and count-sum for melb

--- TwitterHarvester/view.py
+++ TwitterHarvester/view.py
@@ -13,10 +13,17 @@ SERVER = "http://admin:admin@localhost:5984"
 server = couchdb.Server(SERVER)
 
 hist = server["historic_melb"]
+melb = server["melb_db"]
 
-map_sa3_polarity = """
+map_sa_polarity = """
 function (doc) {
     emit([doc.sa4, doc.sa3, doc.sa2], doc.polarity_score);
+}
+"""
+
+map_id_polarity = """
+function (doc) {
+    emit([doc.id], doc.polarity_score);
 }
 """
 
@@ -38,7 +45,68 @@ function (keys, values, rereduce) {
 }
 """
 
-sa_polarity = design.ViewDefinition(
-    "geo", "sa_polarity", map_sa3_polarity, reduce_sum_count
+reduce_max_id = """
+function (keys, values, rereduce) {
+    results = {"max": -1, "id": null};
+    if (rereduce) {
+        for (var i = 0; i < values.length; i++) {
+            if (values[i].max >= results.max) {
+                results.max = values[i].max;
+                results.id = values[i].id;
+            }
+        }
+    } else {
+        for (var j = 0; j < values.length; j++) {
+            if (values[j] >= results.max) {
+                results.max = values[j];
+                results.id = keys[i];
+            }
+        }
+    }
+    return results;
+}
+"""
+
+reduce_min_id = """
+function (keys, values, rereduce) {
+    results = {"min": -1, "id": null};
+    if (rereduce) {
+        for (var i = 0; i < values.length; i++) {
+            if (values[i].min >= results.min) {
+                results.min = values[i].min;
+                results.id = values[i].id;
+            }
+        }
+    } else {
+        for (var j = 0; j < values.length; j++) {
+            if (values[j] >= results.min) {
+                results.min = values[j];
+                results.id = keys[i];
+            }
+        }
+    }
+    return results;
+}
+"""
+
+sa_polarity_sum_count = design.ViewDefinition(
+    "geo", "sa_polarity_sum_count", map_sa_polarity, reduce_sum_count
 )
-sa_polarity.sync(hist)
+sa_polarity_sum_count.sync(hist)
+
+sa_polarity_sum_count = design.ViewDefinition(
+    "geo", "polarity_sum_count", map_id_polarity, reduce_sum_count
+)
+sa_polarity_sum_count.sync(melb)
+
+polarity_max_id = design.ViewDefinition(
+    "geo", "polarity_max_id", map_id_polarity, reduce_max_id
+)
+polarity_max_id.sync(hist)
+polarity_max_id.sync(melb)
+
+polarity_min_id = design.ViewDefinition(
+    "geo", "polarity_min_id", map_id_polarity, reduce_min_id
+)
+polarity_max_id.sync(hist)
+polarity_min_id.sync(melb)
