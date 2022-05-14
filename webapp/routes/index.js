65e7fe72a87e0663658590ab1a7a2c7e11a6ace6
Charles <HongweiChen38@gmail.com>
2022-05-15 00:59:39 +1000

    Read database's ip address from file

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -2,10 +2,22 @@ var express = require('express');
 const { json } = require('express/lib/response');
 var router = express.Router();
 
+const fs = require('fs');
+
+let dbip;
+fs.readFile('../ansible/vars/setup.yaml', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  dbip = data.match(/(\d+\.){3}\d+/g)[0];
+});
+
 const key = 'AIzaSyDqODW7h13yD9PzJ4wv2y8EJ_pdhy71vp0';
 
 router.get('/', function (req, res, next) {
   res.render('index', {
+    dbip: dbip,
     title: 'Yarra Tram Traffic Sentiment Analysis',
     key: key
   });
