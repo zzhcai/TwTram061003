7c9df475825194f25d6669e37420614627022331
zzhcai <87593086+zzhcai@users.noreply.github.com>
2022-05-14 18:26:03 +1000

    Cleanup

--- TwitterHarvester/harvester1_search.sh
+++
@@ -1,3 +0,0 @@
-# export query='(Balaclava OR "Box hill" OR Camberwell OR Brunswick OR Malvern OR Docklands OR Flinders OR Footscray OR Moreland OR "Port Melbourne" OR "St Kilda" OR Toorak OR Lygon OR Brunswick OR Caulfield OR "La Trobe" OR Collingwood OR Southbank) (myki OR tram OR trams) lang:en -is:retweet'
-
-python3 search.py -q '(Balaclava OR "Box hill" OR Camberwell OR Brunswick OR Malvern OR Docklands OR Flinders OR Footscray OR Moreland OR "Port Melbourne" OR "St Kilda" OR Toorak OR Lygon OR Brunswick OR Caulfield OR "La Trobe" OR Collingwood OR Southbank) (myki OR tram OR trams OR PTV) lang:en -is:retweet' -d melb_db
--- TwitterHarvester/harvester2_search.sh
+++
@@ -1,2 +0,0 @@
-
-python3 search.py -q '(Melbourne OR "Federation Square" OR "Luna Park" OR "Melbourne Cricket Ground" OR "Melbourne Museum" OR "Melbourne Zoo" OR "Melbourne Showgrounds" OR "Victoria Market" OR "National Gallery of Victoria" OR "Royal Botanic Gardens" OR "Royal Exhibition Building" OR Monash OR RMIT OR Deakin OR Swinburne OR "Yarra River" OR "Shrine of Remembrance" OR Flagstaff OR swanston OR "Curtin House" OR "Little Bourke" OR "Melbourne Aquarium" OR "Little Collins") (myki OR tram OR trams OR PTV) lang:en -is:retweet' -d melb_db
