48a9b7c86741f5d5323736d024046c2ade82d826
Bingzhe Jin <jbzjbz@126.com>
2022-05-16 15:17:16 +1000

    Tooltip bug fixed, time wise min max tweet
    Tooltip cover listener fixed, min max tweets showed for time wise comparison

--- webapp/public/javascripts/constants.js
+++ webapp/public/javascripts/constants.js
@@ -1,25 +1,27 @@
 webapp = {
-	server: 'http://172.26.130.6:5984/',
-	db: 'historic_melb',
-	mel_db: 'melb_db',
-	view_avg: 'sa_sum_count',
-	view_max: 'max_id',
-	view_min: 'min_id',
-	view_sa_max: 'sa_max_id',
-	view_sa_min: 'sa_min_id',
-	geoJSON: n => `/jsons/${n}.json`,
-	sa2gl: {
-		'sa2': 3,
-		'sa3': 2,
-		'sa4': 1,
-	},
-	viewURL(design, view, sa) {
-		return `${this.server}${this.db}/_design/${design}/_view/${view}?group_level=${this.sa2gl[sa]}`
-	},
-	hisViewURL(design) {
-		return `${this.server}${this.db}/_design/${design}/_view/sa_sum_count`
-	},
-	melViewURL(design) {
-		return `${this.server}${this.mel_db}/_design/${design}/_view/sum_count`
-	}
-}
+  server: "http://172.26.130.6:5984/",
+  db: "historic_melb",
+  mel_db: "melb_db",
+  view_avg: "sa_sum_count",
+  view_max: "max_id",
+  view_min: "min_id",
+  view_sum: "sum_count",
+  view_sa_max: "sa_max_id",
+  view_sa_min: "sa_min_id",
+  geoJSON: (n) => `/jsons/${n}.json`,
+  sa2gl: {
+    sa2: 3,
+    sa3: 2,
+    sa4: 1,
+    all: 0,
+  },
+  viewURL(design, view, sa) {
+    return `${this.server}${this.db}/_design/${design}/_view/${view}?group_level=${this.sa2gl[sa]}`;
+  },
+  hisViewURL(design) {
+    return `${this.server}${this.db}/_design/${design}/_view/sa_sum_count`;
+  },
+  melViewURL(design, view) {
+    return `${this.server}${this.mel_db}/_design/${design}/_view/${view}`;
+  },
+};
--- webapp/public/javascripts/index.js
+++ webapp/public/javascripts/index.js
@@ -3,9 +3,14 @@ webapp.geoJSONData = {};
 webapp.features = [];
 
 document.getElementById("topic").addEventListener("change", onTopicChange);
-document.getElementById("designSelector").addEventListener("change",
-  () => document.getElementById("topic").selectedOptions[0].value === "Geo-wise comparison" ?
-    webapp.colourAreas() : drawChart());
+document
+  .getElementById("designSelector")
+  .addEventListener("change", () =>
+    document.getElementById("topic").selectedOptions[0].value ===
+    "Geo-wise comparison"
+      ? webapp.colourAreas()
+      : drawChart()
+  );
 
 google.charts.load("current", { packages: ["corechart"] });
 // google.charts.setOnLoadCallback(drawChart);
@@ -13,9 +18,9 @@ google.charts.load("current", { packages: ["corechart"] });
 async function myFetch(url) {
   let json = await fetch(url, {
     headers: {
-      'Authorization': 'Basic ' + btoa('admin:admin'),
+      Authorization: "Basic " + btoa("admin:admin"),
     },
-  }).then(r => r.json());
+  }).then((r) => r.json());
   return json;
 }
 
@@ -28,33 +33,43 @@ async function getHistExtremeID() {
   let json = await myFetch(url);
   for (var r of json.rows) {
     var name = r.key[r.key.length - 1];
-    webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_max][name] = r.value.id;
+    webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_max][name] =
+      r.value.id;
   }
   webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_min] = {};
   url = webapp.viewURL(webapp.design, webapp.view_sa_min, webapp.sa);
   json = await myFetch(url);
   for (var r of json.rows) {
     var name = r.key[r.key.length - 1];
-    webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_min][name] = r.value.id;
+    webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_min][name] =
+      r.value.id;
   }
 }
 
 async function getHistTwitter(id) {
-  let url = webapp.server + webapp.db + '/' + id;
+  let url = webapp.server + webapp.db + "/" + id;
   let json = await myFetch(url);
-  return { score: json[webapp.design + '_score'].toFixed(3), text: json.text };
+  return { score: json[webapp.design + "_score"].toFixed(3), text: json.text };
 }
 
 async function getCurrTwitter(id) {
-  let url = webapp.server + webapp.mel_db + '/' + id;
+  let url = webapp.server + webapp.mel_db + "/" + id;
   let json = await myFetch(url);
-  return { score: json[webapp.design + '_score'].toFixed(3), text: json.text };
+  return { score: json[webapp.design + "_score"].toFixed(3), text: json.text };
 }
 
 function showTweets(area, maxT, minT) {
-  document.getElementById('area').innerHTML = area;
-  document.getElementById('maxT').innerHTML = "Max score: " + maxT.score + ' - ' + maxT.text;
-  document.getElementById('minT').innerHTML = "Min score: " + minT.score + ' - ' + minT.text;
+  document.getElementById("area").innerHTML = area;
+  document.getElementById("maxT").innerHTML =
+    "Max score: " + maxT.score + " - " + maxT.text;
+  document.getElementById("minT").innerHTML =
+    "Min score: " + minT.score + " - " + minT.text;
+}
+
+function showTweets2(maxT, minT) {
+  document.getElementById("area").innerHTML = "";
+  document.getElementById("maxT").innerHTML = maxT;
+  document.getElementById("minT").innerHTML = minT;
 }
 
 function onTopicChange() {
@@ -90,40 +105,75 @@ async function drawChart() {
   //     ],
   //   ]);
 
-  webapp.design = document.getElementById("designSelector").selectedOptions[0].value;
+  webapp.design =
+    document.getElementById("designSelector").selectedOptions[0].value;
   var data = google.visualization.arrayToDataTable([
     ["Year", "Average Score", { role: "style" }],
     [
       "2014-15",
-      parseFloat((await fetch(webapp.hisViewURL(webapp.design), {
-        headers: { Authorization: "Basic " + btoa("admin:admin") },
-      }).then(r => r.json()).then(json => json.rows[0].value.sum / json.rows[0].value.count))
-        .toFixed(3)),
+      parseFloat(
+        (
+          await fetch(webapp.hisViewURL(webapp.design), {
+            headers: { Authorization: "Basic " + btoa("admin:admin") },
+          })
+            .then((r) => r.json())
+            .then((json) => json.rows[0].value.sum / json.rows[0].value.count)
+        ).toFixed(3)
+      ),
       "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF",
     ],
     [
       "2022",
-      parseFloat((await fetch(webapp.melViewURL(webapp.design), {
-        headers: { Authorization: "Basic " + btoa("admin:admin") },
-      }).then(r => r.json()).then(json => json.rows[0].value.sum / json.rows[0].value.count))
-        .toFixed(3)),
+      parseFloat(
+        (
+          await fetch(webapp.melViewURL(webapp.design, webapp.view_sum), {
+            headers: { Authorization: "Basic " + btoa("admin:admin") },
+          })
+            .then((r) => r.json())
+            .then((json) => json.rows[0].value.sum / json.rows[0].value.count)
+        ).toFixed(3)
+      ),
       "stroke-color: #76A7FA; stroke-width: 4; fill-color: #76A7FA; fill-opacity: 0.4",
     ],
   ]);
 
   var options = {
     title: "2014-15 vs 2022 traffic tweets sentiment comparison",
-    legend: { position: 'none' },
+    legend: { position: "none" },
     vAxis: {
-      title: 'score',
+      title: "score",
       // ticks: [-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1]
-    }
+    },
   };
 
   var chart = new google.visualization.ColumnChart(
     document.getElementById("chart")
   );
   chart.draw(data, options);
+
+  let his_max_id = await myFetch(
+    webapp.viewURL(webapp.design, webapp.view_sa_max, "all")
+  );
+  let his_min_id = await myFetch(
+    webapp.viewURL(webapp.design, webapp.view_sa_min, "all")
+  );
+
+  let cur_max_id = await myFetch(
+    webapp.melViewURL(webapp.design, webapp.view_max)
+  );
+  let cur_min_id = await myFetch(
+    webapp.melViewURL(webapp.design, webapp.view_min)
+  );
+
+  let his_max = await getHistTwitter(his_max_id.rows[0].value.id);
+  let his_min = await getHistTwitter(his_min_id.rows[0].value.id);
+
+  let cur_max = await getCurrTwitter(cur_max_id.rows[0].value.id);
+  let cur_min = await getCurrTwitter(cur_min_id.rows[0].value.id);
+  showTweets2(
+    `2014-15: </br> Max score: ${his_max.score} - ${his_max.text} </br></br>Min score: ${his_min.score} - ${his_min.text}`,
+    `</br>2022: </br> Max score: ${cur_max.score} - ${cur_max.text} </br></br>Min score: ${cur_min.score} - ${cur_min.text}`
+  );
 }
 
 webapp.init = function () {
@@ -160,11 +210,15 @@ webapp.init = function () {
   webapp.map.data.addListener("click", async function (event) {
     let name = getName(event.feature);
     if (name in webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_max]) {
-      let maxT = await getHistTwitter(webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_max][name]);
-      let minT = await getHistTwitter(webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_min][name]);
+      let maxT = await getHistTwitter(
+        webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_max][name]
+      );
+      let minT = await getHistTwitter(
+        webapp.viewData[webapp.sa][webapp.design][webapp.view_sa_min][name]
+      );
       showTweets(name, maxT, minT);
     }
-  })
+  });
   webapp.show();
 };
 document.getElementById("saSelector").addEventListener("change", webapp.init);
@@ -221,8 +275,8 @@ function getName(feature) {
 function showTooltip(text, pos) {
   let tt = document.getElementById("tooltip");
   let ttt = document.getElementById("tooltiptext");
-  tt.style.left = pos.x + "px";
-  tt.style.top = pos.y + "px";
+  tt.style.left = pos.x + 5 + "px";
+  tt.style.top = pos.y + 5 + "px";
   ttt.innerHTML = text;
   ttt.style.visibility = "visible";
 }
@@ -242,7 +296,8 @@ async function fetchGeoJSON() {
 }
 
 async function fetchView() {
-  webapp.design = document.getElementById("designSelector").selectedOptions[0].value;
+  webapp.design =
+    document.getElementById("designSelector").selectedOptions[0].value;
   if (
     webapp.viewData[webapp.sa] &&
     webapp.viewData[webapp.sa][webapp.design] &&
@@ -250,7 +305,9 @@ async function fetchView() {
   ) {
     return;
   }
-  let data = await myFetch(webapp.viewURL(webapp.design, webapp.view_avg, webapp.sa));
+  let data = await myFetch(
+    webapp.viewURL(webapp.design, webapp.view_avg, webapp.sa)
+  );
   data = data.rows.map((r) => ({
     k: r.key[r.key.length - 1],
     v: r.value.sum / r.value.count,
